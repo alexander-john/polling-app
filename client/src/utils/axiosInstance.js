1d0edc21eaa18859c3f684b002e3fe6b153d6756
Alexander John <96aljohn@gmail.com>
2025-02-28 18:38:17 -0800

    add comments to axiosInstance file

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,25 +1,29 @@
 import axios from "axios";
 import { BASE_URL } from "./apiPaths";
 
+// create an axios instance with default configuration
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
-  timeout: 10000,
+  baseURL: BASE_URL,  // base URL for all queries
+  timeout: 10000, // timeout for requests
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    "Content-Type": "application/json", // default content type
+    Accept: "application/json", // default accept type
   },
 });
 
 // Request Interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
+    // get the access token from local storage
     const accessToken = localStorage.getItem("token");
     if (accessToken) {
+      // if token exists, add it to the Authorization header
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
-    return config;
+    return config;  // return the modified config
   },
   (error) => {
+    // handle request error
     return Promise.reject(error);
   }
 );
@@ -27,6 +31,7 @@ axiosInstance.interceptors.request.use(
 // Response Interceptor
 axiosInstance.interceptors.response.use(
   (response) => {
+    // return the response data if the request is successful
     return response;
   },
   (error) => {
@@ -38,11 +43,14 @@ axiosInstance.interceptors.response.use(
         // Redirect to login page
         window.location.href = "/login";
       } else if (error.response.status === 500) {
+        // internal server error
         console.error("Server error. Please try again later.");
       }
     } else if (error.code === "ECONNABORTED") {
+      // request timeout
       console.error("Request timeout. Please try again.");
     }
+    // reject the promise with the error object
     return Promise.reject(error);
   }
 );
