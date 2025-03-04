9cec872210934e3ac9123619ae24e9ca6fd520b1
Alexander John <96aljohn@gmail.com>
2025-03-03 21:21:08 -0800

    add comments and format to User file

--- server/models/User.js
+++ server/models/User.js
@@ -1,25 +1,33 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+// Import necessary modules
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  fullName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  profileImageUrl: { type: String, default: null },
-  bookmarkedPolls: [{ type: mongoose.Schema.Types.ObjectId, ref: "Poll" }],
-}, { timestamps: true });
+// Define the User schema
+const UserSchema = new mongoose.Schema(
+    {
+        username: { type: String, required: true }, // Username of the user
+        fullName: { type: String, required: true }, // Full name of the user
+        email: { type: String, required: true, unique: true }, // Email of the user, must be unique
+        password: { type: String, required: true }, // Password of the user
+        profileImageUrl: { type: String, default: null }, // URL of the user's profile image
+        bookmarkedPolls: [
+            { type: mongoose.Schema.Types.ObjectId, ref: "Poll" }, // Array of bookmarked polls
+        ],
+    },
+    { timestamps: true } // Automatically add createdAt and updatedAt timestamps
+);
 
 // Hash password before saving
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+UserSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next(); // If password is not modified, skip hashing
+    this.password = await bcrypt.hash(this.password, 10); // Hash the password with a salt round of 10
+    next(); // Proceed to the next middleware or save the document
 });
 
 // Compare passwords
 UserSchema.methods.comparePassword = async function (candidatePassword) {
-  return await bcrypt.compare(candidatePassword, this.password);
+    return await bcrypt.compare(candidatePassword, this.password); // Compare the candidate password with the hashed password
 };
 
-module.exports = mongoose.model('User', UserSchema);
+// Export the User model
+module.exports = mongoose.model("User", UserSchema);
