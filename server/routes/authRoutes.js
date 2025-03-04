d6e88952a4bb6a6896989fbca6b26f64fbe7240f
Alexander John <96aljohn@gmail.com>
2025-03-03 21:06:35 -0800

    add comments to authRoutes file

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,26 +1,35 @@
+// Import necessary modules and controllers
 const express = require("express");
 const {
-  registerUser,
-  loginUser,
-  getUserInfo
+    registerUser,
+    loginUser,
+    getUserInfo,
 } = require("../controllers/authController");
 const { protect } = require("../middleware/authMiddleware");
 const upload = require("../middleware/uploadMiddleware");
 
+// Create a new router instance
 const router = express.Router();
 
+// Route to register a new user
 router.post("/register", registerUser);
+
+// Route to log in a user
 router.post("/login", loginUser);
+
+// Route to get user information (protected route)
 router.get("/getUser", protect, getUserInfo);
 
+// Route to upload an image
 router.post("/upload-image", upload.single("image"), (req, res) => {
-  if (!req.file) {
-    return res.status(400).json({ message: "No file uploaded" });
-  }
-  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
-    req.file.filename
-  }`;
-  res.status(200).json({ imageUrl });
+    if (!req.file) {
+        return res.status(400).json({ message: "No file uploaded" });
+    }
+    const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+        req.file.filename
+    }`;
+    res.status(200).json({ imageUrl });
 });
 
+// Export the router
 module.exports = router;
