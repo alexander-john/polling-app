c7b1053f96addc3d52bdd6a17066c2f552ba0bb8
Alexander John <96aljohn@gmail.com>
2025-03-03 20:33:11 -0800

    add comments and fix typos to SideMenu file

--- client/src/components/layout/SideMenu.jsx
+++ client/src/components/layout/SideMenu.jsx
@@ -4,31 +4,35 @@ import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
 
 const SideMenu = ({ activeMenu }) => {
+  // Get the clearUser function from UserContext
   const { clearUser } = useContext(UserContext);
   const navigate = useNavigate();
 
+  // Handle menu item click
   const handleClick = (route) => {
     if (route === "logout") {
-      handelLogout();
+      handleLogout();
       return;
     }
 
     navigate(route);
   };
 
-  const handelLogout = () => {
-    localStorage.clear();
-    clearUser();
-    navigate("/login");
+  // Handle logout
+  const handleLogout = () => {
+    localStorage.clear(); // Clear local storage
+    clearUser(); // Clear user context
+    navigate("/login"); // Navigate to login page
   };
 
   return (
     <div className="w-64 h-[calc(100vh-61px)] bg-slate-50/50 border-r border-slate-100/70 p-5 sticky top-[61px] z-20">
+      {/* Render side menu items */}
       {SIDE_MENU_DATA.map((item, index) => (
         <button
           key={`menu_${index}`}
           className={`w-full flex items-center gap-4 text-[15px] ${
-            activeMenu == item.label ? "text-white bg-primary" : ""
+            activeMenu === item.label ? "text-white bg-primary" : ""
           } py-4 px-6 rounded-full mb-3`}
           onClick={() => handleClick(item.path)}
         >
