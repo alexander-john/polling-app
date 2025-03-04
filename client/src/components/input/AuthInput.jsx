6a3540ab8566d4c59de864eed571e0bce908a65c
Alexander John <96aljohn@gmail.com>
2025-03-03 20:44:51 -0800

    add comments to Authinput file

--- client/src/components/input/AuthInput.jsx
+++ client/src/components/input/AuthInput.jsx
@@ -2,20 +2,24 @@ import React, { useState } from "react";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
 const AuthInput = ({ value, onChange, label, placeholder, type }) => {
+  // State to manage the visibility of the password
   const [showPassword, setShowPassword] = useState(false);
 
+  // Function to toggle the visibility of the password
   const toggleShowPassword = () => {
     setShowPassword(!showPassword);
   };
 
   return (
     <div>
+      {/* Label for the input field */}
       <label className="text-[13px] text-slate-800">{label}</label>
 
       <div className="input-box">
+        {/* Input field */}
         <input
           type={
-            type == "password" ? (showPassword ? "text" : "password") : "text"
+            type === "password" ? (showPassword ? "text" : "password") : "text"
           }
           placeholder={placeholder}
           className="w-full bg-transparent outline-none"
@@ -23,6 +27,7 @@ const AuthInput = ({ value, onChange, label, placeholder, type }) => {
           onChange={(e) => onChange(e)}
         />
 
+        {/* Toggle password visibility icon */}
         {type === "password" && (
           <>
             {showPassword ? (
